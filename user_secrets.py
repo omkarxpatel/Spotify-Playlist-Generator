77e10e7b4b1e6e50537e7640a1ee116f9300edf9
omkarxpatel <bendonveryt@gmail.com>
2023-01-04 15:10:30 -0800

    fixed gitignore

---
+++ user_secrets.py
@@ -0,0 +1,12 @@
+username = "9mjw6j7712eqyhu54vu5dsag6"
+clientID = "fbe9c56baeb24b4e83331867cb867e34"
+clientSecret = "20d4c1e7719641ccb02559bedcdbd7b2"
+redirectURI = "https://open.spotify.com" 
+
+banner = """Welcome, {}!
+---------------------------------------
+| 0 | Exit                            |
+| 1 | Play a Song                     |
+| 2 | Get a song recommendation       |
+| 3 | Generate a new playlist         |
+---------------------------------------"""
