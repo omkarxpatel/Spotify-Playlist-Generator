be1e0cefe26831ec5c6c00e868bcefb64c41ea23
omkarxpatel <bendonveryt@gmail.com>
2023-01-03 20:07:44 -0800

    fixed secrets

--- main.py
+++ main.py
@@ -6,20 +6,15 @@ import os
 import time
 import spotipy
 import webbrowser
-from colorama import Fore as fore
 import spotipy.util as util
+from colorama import Fore as fore
 
 
 ##################################
 #   CONFIG VALUES -> README.md   #
 ##################################
 
-
-username = "9mjw6j7712eqyhu54vu5dsag6"
-clientID = "fbe9c56baeb24b4e83331867cb867e34"
-clientSecret = "20d4c1e7719641ccb02559bedcdbd7b2"
-redirectURI = "https://open.spotify.com" 
-
+from user_secrets import username, clientID, clientSecret, redirectURI
 
 ########################
 #   HELPER FUNCTIONS   #
