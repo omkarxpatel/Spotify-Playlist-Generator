0c4ca171267a1ff6157cf596b6b79601a451f19a
omkarxpatel <bendonveryt@gmail.com>
2023-01-07 15:02:04 -0800

    added user secrets example

---
+++ utils/user_secrets_example.py
@@ -0,0 +1,11 @@
+username = "username"
+clientID = "client_id"
+clientSecret = "client_secret"
+redirectURI = "your_redirect_url" 
+
+banner = """Welcome, {}!
+---------------------------------------
+| 1 | Play a Song by its name         |
+| 2 | Get a song recommendation       |
+| 3 | Generate a new playlist         |
+---------------------------------------"""
